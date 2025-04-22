9cb94110f4d46bd21c53371f6111a822eae5606c
Riley Boughner <jojo.picktwn@gmail.com>
2025-04-22 12:45:17 -0400

    updates

--- config/nixos/hosts/server.nix
+++ config/nixos/hosts/server.nix
@@ -11,11 +11,4 @@
  virtualisation.docker.enable = true;
  
  services.openssh.enable = true;
- services.openssh.settings = {
-   permitRootLogin = "no";         # Safer to disable root login
-   passwordAuthentication = true; # Use SSH keys instead
- };
-
-
-
 }

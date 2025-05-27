6cf7f29b8c565c3f1cb78c8e94ff58d4dd272785
Riley Boughner <jojo.picktwn@gmail.com>
2025-05-27 14:38:26 -0400

    server updates

--- config/nixos/hosts/server.nix
+++ config/nixos/hosts/server.nix
@@ -4,8 +4,20 @@
   # -- hostname --
   networking.hostName = "clownweb";
 
+  # -- ports --
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  # -- mounts --
+    fileSystems."/mnt/tank" = {
+      device = "UUID=76b26a47-20a0-483e-a02d-3154c16779aa";
+      fsType = "btrfs";
+      options = [ "degraded" "nofail" "compress=zstd" ];
+    };
+
   # -- packages --
-  environment.systemPackages = with pkgs; [];
+  environment.systemPackages = with pkgs; [
+    btrfs-progs
+  ];
   
  # -- services --
  virtualisation.docker.enable = true;
--- config/nixos/server-modules/vaultwarden.nix
+++ config/nixos/server-modules/vaultwarden.nix
@@ -1,22 +1,7 @@
 { config, pkgs, lib, ... }: 
 
 {
- 
-services.vaultwarden = {
-  enable = true;
-  dbBackend = "postgresql";
-  environmentFile = "/home/admin/vaultwarden/.env";
-  config = {
-    ROCET_PORT = 8222;
-    SIGNUPS_ALLOWED = false;
-    DOMAIN = "https://passwords.clownweb.net";
+  services.vaultwarden = {
+    enable = true;
   };
-};
-
-  services.caddy = {
-    virtualHosts."passwords.clownweb.net".extraConfig = ''
-      reverse_proxy http://localhost:8222
-    '';
-  };
-
-  }
+}

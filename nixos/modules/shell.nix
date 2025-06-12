8eea65c7d915e86325417c6f765309ae66011558
Riley Boughner <riley@clownweb.net>
2025-06-12 13:28:21 -0400

    Manual update on desktop

--- nixos/modules/shell.nix
+++ nixos/modules/shell.nix
@@ -16,8 +16,6 @@
 
       rebuild = "~/.system/scripts/rebuild";
       update = "~/.system/scripts/update";
-      push = "~/.system/scripts/push";
-      pull = "~/.system/scripts/pull";
 
       sync = "~/.system/scripts/sync";
       etch = "~/.system/scripts/etch";

11a20fee96efe516d79a23022eeca21bb9d50df5
Riley Boughner <riley@clownweb.net>
2025-07-03 13:41:08 -0400

    Manual update on desktop

--- nixos/modules/docker.nix
+++ nixos/modules/docker.nix
@@ -1,4 +1,5 @@
-{ config, pkgs, lib, ... }: 
+{ config, pkgs, lib, ... }:
+
 let
   cfg = config.features.docker;
 in
@@ -10,18 +11,14 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    #enable docker
     virtualisation.docker = {
       enable = true;
       enableOnBoot = true;
       package = pkgs.docker_25;
     };
 
-    #add the user to the docker group
-    #users.users.${config.user.username}.extraGroups = [ "docker" ];
-
-    #enable the nvidia tooklit if nvidia is enabled
+    # Optional: only enable if NVIDIA is also enabled
     hardware.nvidia-container-toolkit.enable = lib.mkIf config.features.nvidia.enable true;
-    #hardware.nvidia-container-toolkit.enable = true;
   };
 }
+
--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,23 +1,22 @@
-{ config, pkgs, lib, ... }: 
+{ config, pkgs, lib, ... }:
+
 let
   cfg = config.features.nvidia;
 in
 {
-  
   options.features.nvidia.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
-    description = "Enable Nvidia drivers and settings";
+    description = "Enable NVIDIA drivers and support.";
   };
 
   config = lib.mkIf cfg.enable {
-    hardware.graphics.enable = true;
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
       modesetting.enable = true;
-      nvidiaSettings = true;
       open = false;
+      nvidiaSettings = true;
     };
   };
 }
+

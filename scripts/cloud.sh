eaaef79c1026e4ba899dee3ba293dbf2acbd7151
Riley Boughner <riley@clownweb.net>
2025-12-04 08:49:27 -0500

    updated cloud script back to Documents folder

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p sshfs
 
-sshfs admin@192.168.1.2:/mnt/tank/cloud/Documents ~/documents
+sshfs admin@192.168.1.2:/mnt/tank/cloud/Documents ~/Documents

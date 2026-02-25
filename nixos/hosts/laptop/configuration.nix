2c522483298439e523a866181a9c7406ff637d96
Riley Boughner <riley@clownweb.net>
2026-02-25 15:53:00 -0500

    updated configuration to use hardware config in /etc/nixos

--- nixos/hosts/laptop/configuration.nix
+++ nixos/hosts/laptop/configuration.nix
@@ -3,21 +3,22 @@
 {
   imports =
     [
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
       ../../configuration.nix
       ../../modules/docker.nix
+      ../../modules/kubernetes.nix
       ../../modules/wireless-networking.nix
     ];
 
   networking.hostName = "laptop";
 
   # -- auto vpn --
-  networking.wg-quick.interfaces = {
-    clownweb = {
-      configFile = "/etc/wireguard/clownweb.conf";
-      autostart = true;
-    };
-  };
+  #networking.wg-quick.interfaces = {
+  #   clownweb = {
+  #   configFile = "/etc/wireguard/clownweb.conf";
+  #   autostart = true;
+  #  };
+  #};
 
   # -- fingerprint --
   services.fprintd.enable = true;
